4d926bacb81aedb96079e7604ae53501d1d58cb6
James Chang <jc4095@nyu.edu>
2023-12-11 00:06:11 -0500

    Add apartment.html page, update canned.html, other small tweaks

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,23 @@ export default {
   pages: {
     "404": {
       title: "James Chang - Entrepreneur, builder, angel investor",
+      lastUpdated: "N/A",
     },
     index: {
       title: "James Chang - Entrepreneur, builder, angel investor",
+      lastUpdated: new Date("2023-12-11").toDateString(),
     },
     canned: {
       title: "James Chang - Entrepreneur, builder, angel investor",
+      lastUpdated: new Date("2023-12-11").toDateString(),
+    },
+    apartment: {
+      title: "James Chang - Entrepreneur, builder, angel investor",
+      lastUpdated: new Date("2023-12-11").toDateString(),
     },
     contact: {
       title: "James Chang - Entrepreneur, builder, angel investor",
+      lastUpdated: new Date("2023-12-11").toDateString(),
     },
   },
 };
